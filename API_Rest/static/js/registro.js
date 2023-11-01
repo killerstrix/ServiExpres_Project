fe9f7958bc701c8b6cafb6500e44f1af7d901356
killerstrix <127537747+killerstrix@users.noreply.github.com>
2023-11-01 13:11:02 -0300

    Project_Serviexpress

---
+++ API_Rest/static/js/registro.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    $("#btnvolverlogin").click(function () {
+        window.location.href = "login.html";
+    });
+});
+
+$("#btnCrearCuenta").click(function (e) {
+    var erroresR = validarR();
+    if (erroresR !== "") {
+        Swal.fire("Error de envío", erroresR, "error");
+    } else {
+        Swal.fire("Datos Validos", "Cuenta creada con exito", "success")
+            .then(function () {
+                window.location.href = "login.html";
+
+            });
+    }
+    e.preventDefault();
+});
+
+function validarR() {
+    var html = "";
+    var UserName = $("#txtUserName").val();
+    var Password = $("#txtPassword").val();
+    var correo = $("#txtCorreoC").val();
+    var NumTel = $("#txtNumberTelephone").val();
+    var radioMasculino = document.querySelector("#rbtMasculino");
+    var RadioFemenino = document.querySelector("#rbtFemenino");
+    var RadioOtros = document.querySelector("#rbtOtros");
+    var radio = document.querySelector("#ValTerms");
+
+    if (UserName === "") {
+        html += "-Debe ingresar el nombre de usuario<br>";
+    } else if (!UserName.length > 7) {
+        html += "-El nombre de usuario debe tener al menos 8 caracteres<br>";
+    }
+
+    if (Password === "") {
+        html += "-Debe ingresar la contraseña<br>";
+    }else if (!Password.match(/^[a-zA-Z0-9_-]{8,}$/)) {
+        html += "-La contraseña debe tener al menos 8 caracteres y solo debe contener letras, números, guiones bajos y guiones<br>";
+    }
+
+    if (correo === "") {
+        html += "-Debe ingresar el correo electrónico<br>";
+    }else if(correo === "" || correo.indexOf("@") === -1) {
+        html += "-Debe ingresar el caracter '@' en el correo electronico<br>";
+    }
+    
+
+    if (NumTel === "" || parseInt(NumTel) > 99999999 || parseInt(NumTel) < 10000000) {
+        html += "-El número telefónico debe tener 8 o 9 dígitos<br>";
+    }
+
+    if (!radioMasculino.checked && !RadioFemenino.checked && !RadioOtros.checked) {
+        html += "-Debe seleccionar un genero<br>";
+    }
+
+    if (!radio.checked) {
+        html += "-Debe aceptar los términos y condiciones<br>";
+    }
+
+    return html;
+}
