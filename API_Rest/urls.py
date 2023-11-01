7239803b46c24d438aa16a3e631d85f42e707808
killerstrix <127537747+killerstrix@users.noreply.github.com>
2023-11-01 17:32:53 -0300

    Menu de empleados funcional

--- API_Rest/urls.py
+++ API_Rest/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import login, registro, LoginEmpleados, index, MenuEmpleados
+from . views import login, registro, LoginEmpleados, index, MenuEmpleados, crud_productos, crud_Servicios, crud_cuentas, Generar_Informes
 
 urlpatterns=[
     path('', login, name="login"),
@@ -7,4 +7,8 @@ urlpatterns=[
     path('LoginEmpleados/', LoginEmpleados, name="LoginEmpleados"),
     path('MenuEmpleados/', MenuEmpleados, name="MenuEmpleados"),
     path('index/', index, name="index"),
+    path('crud_productos/', crud_productos, name="crud_productos"),
+    path('crud_Servicios/', crud_Servicios, name="crud_Servicios"),
+    path('crud_cuentas/', crud_cuentas, name="crud_cuentas"),
+    path('Generar_Informes/', Generar_Informes, name="Generar_Informes"),
 ]
--- API_Rest/views.py
+++ API_Rest/views.py
@@ -19,3 +19,16 @@ def MenuEmpleados(request):
 def index(request):
     return render(request, "core/index.html")
 
+def crud_productos(request):
+    return render(request, "core/crud_productos.html")
+
+def crud_Servicios(request):
+    return render(request, "core/crud_Servicios.html")
+
+def crud_cuentas(request):
+    return render(request, "core/crud_cuentas.html")
+
+def Generar_Informes(request):
+    return render(request, "core/Generar_Informes.html")
+
+
